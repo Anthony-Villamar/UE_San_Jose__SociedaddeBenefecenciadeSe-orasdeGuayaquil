7921ac9387899c3c701d01f9169127802fd03c3b
Anthony-Villamar <anthonyvillamar3000@gmail.com>
2025-09-26 08:43:34 -0500

    backend4

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(session({
     cookie: {
         httpOnly: true,
         secure: true, // ponlo en true si se usa HTTPS y none
-        sameSite: "none", // "none" si usas frontend en otro dominio con HTTPS y lax si es el mismo dominio
+        sameSite: "lax", // "none" si usas frontend en otro dominio con HTTPS y lax si es el mismo dominio
         maxAge: 1000 * 60 * 60 * 7// 7 hora si se quiere una hora quitar el *7
     }
 }));
